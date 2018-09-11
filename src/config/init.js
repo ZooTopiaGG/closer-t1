5d81442d2878f0d4e796d30f4245166ef970cc3c
hennessey <yuhang.wu@tiejin.com>
2018-09-11 16:50:18 +0800

    改一下checkversion

--- src/config/init.js
+++ src/config/init.js
@@ -1,11 +1,11 @@
 import config from './index';
 
-let UA = navigator.userAgent.toLowerCase(),
+let UA = navigator.userAgent.toLowerCase() || navigator.userAgent.toLowerCase(),
   HREF = location.href,
   ENV = window.ENV = {};
 
-function initENV() { 
-  
+function initENV() {
+
   // 根据href值设置当前开发环境（build,sandbox,dev）
   for (let key in config.href) {
     let index = HREF.indexOf(key);
@@ -13,15 +13,15 @@ function initENV() {
   }
 
   ENV.app = /closer-(android|ios)/.test(UA), // 贴近客户端
-  ENV.wx = /micromessenger/.test(UA), // 微信客户端
-  ENV.ios = /((iphone\sos)\s([\d_]+))|(-ios)/.test(UA), // ios
-  ENV.android = /((android);?[\s\/]+([\d.]+)?)|(-android)/.test(UA); // 安卓
+    ENV.wx = /micromessenger/.test(UA), // 微信客户端
+    ENV.ios = /((iphone\sos)\s([\d_]+))|(-ios)/.test(UA), // ios
+    ENV.android = /((android);?[\s\/]+([\d.]+)?)|(-android)/.test(UA); // 安卓
   ENV.weibo = /weibo/.test(UA), // 新浪微博
-  ENV.qq = /qq/.test(UA), // qq浏览器
-  ENV.safari = /version\/([\d.]+)([^S](safari)|[^M]*(mobile)[^S]*(safari))/.test(UA), // safari浏览器
-  ENV.chrome = /chrome\/([\d.]+)/.test(UA) || /crios\/([\d.]+)/.test(UA), // chrome浏览器
-  ENV.firefox = /firefox\/([\d.]+)/.test(UA), // firefox浏览器
-  ENV.ie = /msie\s([\d.]+)/.test(UA) || /trident\/[\d](?=[^\?]+).*rv:([0-9.].)/.test(UA) // ie浏览器
+    ENV.qq = /qq/.test(UA), // qq浏览器
+    ENV.safari = /version\/([\d.]+)([^S](safari)|[^M]*(mobile)[^S]*(safari))/.test(UA), // safari浏览器
+    ENV.chrome = /chrome\/([\d.]+)/.test(UA) || /crios\/([\d.]+)/.test(UA), // chrome浏览器
+    ENV.firefox = /firefox\/([\d.]+)/.test(UA), // firefox浏览器
+    ENV.ie = /msie\s([\d.]+)/.test(UA) || /trident\/[\d](?=[^\?]+).*rv:([0-9.].)/.test(UA) // ie浏览器
 
   // 是否dev环境，非线上环境时均设置dev=true
   ENV.dev = ENV.env != 'build';
@@ -31,8 +31,9 @@ function initENV() {
 
 // 版本号比较
 function checkVersion(ver) {
-  let version = UA.match(/closerapp\/version\/([0-9.]+)/)[1];
+  let version = UA.match(/closerapp\/version\/([0-9.]+)/)
   if (version && ver) {
+    version = version[1]
     version = version.split('.');
     ver = ver.split('.');
     let len = Math.max(version.length, ver.length);
