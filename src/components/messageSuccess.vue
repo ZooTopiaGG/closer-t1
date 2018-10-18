356648af48f5cfc0e310dac82e196eb1a591f17f
徐佳 <jia.xu@tiejin.me>
2018-10-18 15:36:15 +0800

    fix 留言成功弹窗

--- src/components/messageSuccess.vue
+++ src/components/messageSuccess.vue
@@ -1,19 +1,19 @@
 <template>
-<mt-popup v-model="visible2" class="success-pop" :closeOnClickModal="closeOnClickModal">
-    <div class="box box-tb box-center-center">
-      <div class="close-icon" @click="close"></div>
-      <div class="pop-header">
-        <div class="text">恭喜你！留言成功</div>
-        <div class="text">快去app看看其他精彩留言吧~~</div>
-      </div>
-  
-      <div class="pop-footer box box-lr">
-        <div class="wait-btn" @click="goBack">再等等</div>
-        <div class="confirm-btn" @click="downApp">好的</div>
+  <div class="pop box box-center-center" v-if="visible2">
+    <div class="success-pop" :closeOnClickModal="closeOnClickModal">
+      <div class="box box-tb box-center-center">
+        <div class="close-icon" @click="close"></div>
+        <div class="pop-header">
+          <div class="text">恭喜你！留言成功</div>
+          <div class="text">快去app看看其他精彩留言吧~~</div>
+        </div>
+        <div class="pop-footer box box-lr">
+          <div class="wait-btn" @click="goBack">再等等</div>
+          <div class="confirm-btn" @click="downApp">好的</div>
+        </div>
       </div>
     </div>
-  </mt-popup>
-  
+  </div>
 </template>
 
 <script>
@@ -51,7 +51,7 @@
       };
     },
     beforeMount() {
-
+  
     },
     mounted() {
       console.log('params---', this.$route.params)
@@ -64,19 +64,12 @@
     computed: {
       ...mapState("message", {
         visible: state => state.visible
-      }),
-      
-      ...mapState("message", {
-        visible: state => state.visible
       })
     },
     methods: {
       ...mapMutations("message", [
         'show', 'hide'
       ]),
-      open() {
-        this.show()
-      },
       close() {
         this.hide()
       },
@@ -94,57 +87,68 @@
 </script>
 
 <style lang="less" scoped>
-  .success-pop {
-    width: 654pr;
-    border-radius: 10pr;
-    background: #fff;
-    text-align: center;
-    padding: 40pr;
-    color: #4b4945;
-    .close-icon {
-      position: absolute;
-      right: 20pr;
-      top: 20pr;
-      width: 28pr;
-      height: 28pr;
-      &::before,
-      &::after {
-        content: "";
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        width: 2px;
-        height: 28pr;
-        background-color: #9b9b9b;
-      }
-      &::before {
-        transform: translate(-50%, -50%) rotate(-45deg);
-      }
-      &::after {
-        transform: translate(-50%, -50%) rotate(45deg);
-      }
-    }
-    .pop-header {
-      margin-top: 20pr;
-      font-size: 36pr;
-      line-height: 50pr;
-    }
-    .pop-footer {
-      margin-top: 60pr;
-      font-size: 32pr;
-      line-height: 40pr;
+  .pop {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background: rgba(0, 0, 0, .3);
+    .success-pop {
+      position: relative;
+      width: 654pr;
+      border-radius: 10pr;
+      background: #fff;
+      text-align: center;
+      padding: 40pr;
       color: #4b4945;
-      width: 100%;
-      >div {
-        width:  50%;
-        padding: 18pr 0;
+      .close-icon {
+        position: absolute;
+        right: 20pr;
+        top: 20pr;
+        width: 28pr;
+        height: 28pr;
+        &::before,
+        &::after {
+          content: "";
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          width: 2px;
+          height: 28pr;
+          background-color: #9b9b9b;
+        }
+        &::before {
+          transform: translate(-50%, -50%) rotate(-45deg);
+        }
+        &::after {
+          transform: translate(-50%, -50%) rotate(45deg);
+        }
       }
-      .wait-btn {
-        color: #94928E;
+      .pop-header {
+        margin-top: 20pr;
+        font-size: 36pr;
+        line-height: 50pr;
       }
-      .confirm-btn {
-        background: #fddb00;
-        border-radius: 10pr;
+      .pop-footer {
+        margin-top: 60pr;
+        font-size: 32pr;
+        line-height: 40pr;
+        color: #4b4945;
+        width: 100%;
+        >div {
+          width: 50%;
+          padding: 18pr 0;
+        }
+        .wait-btn {
+          color: #94928E;
+        }
+        .confirm-btn {
+          background: #fddb00;
+          border-radius: 10pr;
+        }
       }
     }
   }
