69bd77de8a16ad8ccf609080f0080f4d56a87a9d
徐佳 <jia.xu@tiejin.me>
2018-11-07 14:18:59 +0800

    fix 留言成功弹窗

--- src/pages/message/index/index.vue
+++ src/pages/message/index/index.vue
@@ -108,11 +108,6 @@
 <style lang="less" scoped>
   .write-message {
     width: 100%;
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
     background: #fff;
     padding: 40pr;
     font-size: 36pr;
